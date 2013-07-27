476ac28326b8ac3fd31ee7ff547650ffe203f4d4
Mark T. Holder <mtholder@gmail.com>
2013-07-27 11:52:54 -0400

    working on the same data set that was the basis for the slides

---
+++ test-5.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -x
+mkdir take5 || exit
+cd take5 || exit
+cp ../5-taxa-paup-batch.nex . || exit
+paup -n 5-taxa-paup-batch.nex || exit 
+../parse_paup_sitelike_to_tab_separated.py hcg.likescores.txt hgc.parscores.txt > hcg.score.table.txt || exit
+Rscript ../create_site_lnL_plots.R hcg.score.table.txt || exit
+echo "Done."
