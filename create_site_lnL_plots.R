7582a8c007ef7d920ef92e9e581f8446e7a2b151
Mark T. Holder <mtholder@gmail.com>
2013-07-26 20:20:41 -0400

    scripts for plots relating to the distribution of scores on a pair of trees

---
+++ create_site_lnL_plots.R
@@ -0,0 +1,35 @@
+fn = commandArgs(TRUE)
+d = read.table(fn, header=TRUE, sep="\t");
+parsimony.discriminates = d$PTree1 != d$PTree2;
+pdf(paste(fn, '-diff-by-site.pdf', sep=""));
+diff = d$LTree1 - d$LTree2
+
+plot(d$Site, diff, 
+     xlab="Site", ylab="lnL(T1) - lnL(T2)",
+     type="p",
+     main=sprintf("Difference in lnL across sites.\nTotal lnL(T1) - lnL(T2) = %g ", sum(diff))
+     );
+points(d$Site[parsimony.discriminates],
+    diff[parsimony.discriminates],
+    pch=20,
+    col="red");
+abline(h=0);
+dev.off();
+
+pdf(paste(fn, '-scatterplot.pdf', sep=""));
+plot(d$LTree1, d$LTree2, 
+     xlab="lnL(T1)", ylab="lnL(T2)",
+     type="p",
+     main=sprintf("Scatterplot of site lnL on 2 trees.\nTotal lnL(T1) - lnL(T2) = %g ", sum(diff))
+     );
+points(d$LTree1[parsimony.discriminates],
+    d$LTree2[parsimony.discriminates],
+    pch=20,
+    col="red");
+abline(a=0, b=1); # equality line
+dev.off();
+
+mabs = max(abs(max(diff)), abs(min(diff)))
+pdf(paste(fn, '-diff-histogram.pdf', sep=""));
+hist(diff, seq(-mabs, mabs, mabs/75), xlab=c("lnL(T1) - lnL(T2)"), main="Distribution of the differences in lnL scores");
+dev.off();
--- parse_paup_sitelike_to_tab_separated.py
+++ parse_paup_sitelike_to_tab_separated.py
@@ -1,62 +1,90 @@
 #!/usr/bin/env python
 import sys, os, re
 SCRIPT_NAME = os.path.split(os.path.abspath(sys.argv[0]))[1]
-if len(sys.argv) == 1:
-    inp = iter(sys.stdin)
-elif len(sys.argv) == 2:
-    inp = iter(open(sys.argv[1], 'rU'))
-else:
-    sys.exit(SCRIPT_NAME + ": expecting an input file which should be a paup scorefile produced by LScore with the sitelikes option.\n")
-expected_first = "Tree\t-lnL\tSite\t-lnL"
-first = inp.next().strip()
-if first != expected_first:
-    sys.exit('%s: Expecting "%s" as the first line, but found "%s"\n' % (SCRIPT_NAME, expected_first, first))
 
-TREE_LIKE_PAT = re.compile(r'^(\d+)\t([.0-9]+)')
-SITE_LIKE_PAT = re.compile(r'^\t\t(\d+)\t([.0-9]+)')
-tree_site_ln_like_list = []
-curr_site_ln = []
-curr_site_ln_sum = 0.0
-num_sites = None
-for n, line in enumerate(inp):
-    m = TREE_LIKE_PAT.match(line)
-    if m:
-        if len(curr_site_ln) > 0:
-            assert(len(tree_site_ln_like_list) + 1 == curr_tree_num)
-            if abs(expected_ln_like - curr_site_ln_sum) > 10-4:
-                sys.exit('Tree ln L for tree %d disagrees with sum of site lnL.\n')
-            tree_site_ln_like_list.append(curr_site_ln)
-            if num_sites == None:
-                num_sites = len(curr_site_ln)
-            elif num_sites != len(curr_site_ln):
-                sys.exit('The number of sites for tree %d is %d. Previous tree(s) had %d sites\n' % (curr_tree_num, len(curr_site_ln), num_sites))
-            curr_site_ln= []
-            curr_site_ln_sum = 0.0
-        curr_tree_num = int(m.group(1))
-        expected_ln_like = float(m.group(2))
+def parse_paup_site_like_file(inp, score_type, is_parsimony=False):
+
+    if is_parsimony:
+        expected_first = "Tree\tLength\tCharacter\tLength"
     else:
-        m = SITE_LIKE_PAT.match(line)
-        if not m:
-            sys.exit('%s: Could not parse line %d: "%s"\n' % (SCRIPT_NAME, n + 2, line[:-1]))
-        sn = int(m.group(1))
-        sls = m.group(2)
-        slf = float(sls)
-        assert(sn == (1 + len(curr_site_ln)))
-        curr_site_ln.append(sls)
-        curr_site_ln_sum += slf
+        expected_first = "Tree\t-lnL\tSite\t-lnL"
 
-if len(curr_site_ln) > 0:
-    assert(len(tree_site_ln_like_list) + 1 == curr_tree_num)
-    if abs(expected_ln_like - curr_site_ln_sum) > 10-4:
-        sys.exit('Tree ln L for tree %d disagrees with sum of site lnL.\n')
-    tree_site_ln_like_list.append(curr_site_ln)
-    if num_sites == None:
-        num_sites = len(curr_site_ln)
-    elif num_sites != len(curr_site_ln):
-        sys.exit('The number of sites for tree %d is %d. Previous tree(s) had %d sites\n' % (curr_tree_num, len(curr_site_ln), num_sites))
+    first = inp.next().strip()
+    if first != expected_first:
+        sys.exit('%s: Expecting "%s" as the first line, but found "%s"\n' % (SCRIPT_NAME, expected_first, first))
 
-outp = sys.stdout
-num_trees = len(tree_site_ln_like_list)
-outp.write('Site\ttree%s\n' % '\ttree'.join([str(1 +i) for i in range(num_trees)]))
-for i in range(num_sites):
-    outp.write('%d\t%s\n' % (1 + i, '\t'.join([x[i] for x in tree_site_ln_like_list])))
+    TREE_LIKE_PAT = re.compile(r'^(\d+)\t([.0-9]+)')
+    SITE_LIKE_PAT = re.compile(r'^\t\t(\d+)\t([.0-9]+)')
+    tree_site_ln_like_list = []
+    curr_site_ln = []
+    curr_site_ln_sum = 0.0
+    num_sites = None
+    for n, line in enumerate(inp):
+        m = TREE_LIKE_PAT.match(line)
+        if m:
+            if len(curr_site_ln) > 0:
+                assert(len(tree_site_ln_like_list) + 1 == curr_tree_num)
+                if abs(expected_ln_like - curr_site_ln_sum) > 10-4:
+                    sys.exit('Tree ln L for tree %d disagrees with sum of site lnL.\n')
+                tree_site_ln_like_list.append(curr_site_ln)
+                if num_sites == None:
+                    num_sites = len(curr_site_ln)
+                elif num_sites != len(curr_site_ln):
+                    sys.exit('The number of sites for tree %d is %d. Previous tree(s) had %d sites\n' % (curr_tree_num, len(curr_site_ln), num_sites))
+                curr_site_ln= []
+                curr_site_ln_sum = 0.0
+            curr_tree_num = int(m.group(1))
+            expected_ln_like = score_type(m.group(2))
+        else:
+            m = SITE_LIKE_PAT.match(line)
+            if not m:
+                sys.exit('%s: Could not parse line %d: "%s"\n' % (SCRIPT_NAME, n + 2, line[:-1]))
+            sn = int(m.group(1))
+            sls = m.group(2)
+            slf = score_type(sls)
+            assert(sn == (1 + len(curr_site_ln)))
+            curr_site_ln.append(sls)
+            curr_site_ln_sum += slf
+    if len(curr_site_ln) > 0:
+        assert(len(tree_site_ln_like_list) + 1 == curr_tree_num)
+        if abs(expected_ln_like - curr_site_ln_sum) > 10-4:
+            sys.exit('Tree ln L for tree %d disagrees with sum of site lnL.\n')
+        tree_site_ln_like_list.append(curr_site_ln)
+        if num_sites == None:
+            num_sites = len(curr_site_ln)
+        elif num_sites != len(curr_site_ln):
+            sys.exit('The number of sites for tree %d is %d. Previous tree(s) had %d sites\n' % (curr_tree_num, len(curr_site_ln), num_sites))
+    return tree_site_ln_like_list, num_sites
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        inp = iter(sys.stdin)
+    else:
+        like_filename = sys.argv[1]
+        inp = iter(open(like_filename, 'rU'))
+        if len(sys.argv) > 3:
+            sys.exit(SCRIPT_NAME + ": expecting an input file which should be a paup scorefile produced by LScore with the sitelikes option.\n")
+
+    tree_site_ln_like_list, num_sites = parse_paup_site_like_file(inp, float)
+    pars_filename = None
+    if len(sys.argv) == 3:
+        pars_filename = sys.argv[2]
+        inp = iter(open(pars_filename, 'rU'))
+        tree_site_parsimony_list, num_sitesp = parse_paup_site_like_file(inp, int, is_parsimony=True)
+        assert(num_sites == num_sitesp)
+        assert(len(tree_site_ln_like_list) == len(tree_site_parsimony_list))
+    outp = sys.stdout
+    num_trees = len(tree_site_ln_like_list)
+    if pars_filename:
+        suffix = '\tPTree%s' % '\tPTree'.join([str(1 +i) for i in range(num_trees)])
+    else:
+        suffix = ""
+    lsc_part = '\tLTree'.join([str(1 +i) for i in range(num_trees)])
+    outp.write('Site\tLTree%s%s\n' % (lsc_part, suffix))
+    for i in range(num_sites):
+        if pars_filename:
+            suffix = '\t' + ('\t'.join([x[i] for x in tree_site_parsimony_list]))
+        else:
+            suffix = ""
+        lsc_part = '\t-'.join([x[i] for x in tree_site_ln_like_list])
+        outp.write('%d\t-%s%s\n' % (1 + i, lsc_part, suffix))
