dcf7710c84363c1ab171a04272e936717620a083
Mark T. Holder <mtholder@gmail.com>
2013-07-26 21:08:50 -0400

    ignore file. Works

--- test.sh
+++ test.sh
@@ -4,5 +4,6 @@ mkdir take2 || exit
 cd take2 || exit
 cp ../paup-batch.nex . || exit
 paup -n paup-batch.nex || exit 
-../parse_paup_sitelike_to_tab_seperated.py hcg.likescores.txt hgc.parscores.txt > hcg.score.table.txt || exit
-Rscript create_site_lnL_plots.R hcg.score.table.txt || exit
+../parse_paup_sitelike_to_tab_separated.py hcg.likescores.txt hgc.parscores.txt > hcg.score.table.txt || exit
+Rscript ../create_site_lnL_plots.R hcg.score.table.txt || exit
+echo "Done."
